cb2769b9ba58d9ad08924d7d70265716c272da65
Imran Hossen <freelancerimran81@gmail.com>
2022-04-17 00:37:26 +0600

    header section done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" sticky='top' bg="primary" variant="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                        <span>Fixpro Owner</span>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="home#services">Services</Nav.Link>
+                            <Nav.Link href="home#gallery">Gallery</Nav.Link>
+                            <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                        </Nav>
+                        {/* <Nav>
+                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            {
+                                user ?
+                                    <Nav.Link onClick={handleSignOut}>
+                                        Log Out
+                                    </Nav.Link>
+                                    :
+                                    <Nav.Link as={Link} to="login">
+                                        Login
+                                    </Nav.Link>
+                            }
+                        </Nav> */}
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
